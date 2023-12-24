f6a8a753b2b468bd3e098888363d057a68757b03
Bora Özışık <ozisikbora@gmail.com>
2023-12-25 00:11:01 +0300

    get users data

--- src/views/charts/Charts.tsx
+++ src/views/charts/Charts.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import UserData from "../users.json";
 
 type Props = {};
 
 const Charts = (props: Props) => {
+  console.log("UserData", UserData);
   return <div>Charts</div>;
 };
 
